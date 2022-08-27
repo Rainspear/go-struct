41e5df3a11f514d9525945fd2e2bfca3154e0b88
Khang Phan <khang.phan@opswat.com>
2022-08-28 03:23:39 +0700

    add struct and receiver

--- main.go
+++ main.go
@@ -2,14 +2,38 @@ package main
 
 import "fmt"
 
+type contact struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contact
 }
 
 func main() {
-	kanz := person{firstName: "kanz", lastName: "Han"}
-	fmt.Println(kanz)
-	fmt.Println(kanz.firstName, kanz.lastName)
+	// kanz := person{}
+	// kanz.firstName = "Kanz"
+	// kanz.lastName = "Han"
+	// fmt.Printf("%+v\n", kanz)
+	// fmt.Println(kanz)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Robert",
+		contact: contact{
+			email:   "jimhim@gmail.com",
+			zipCode: 40040,
+		}}
+	jim.print()
+	jim.updateName("John")
+	jim.print()
+}
 
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p person) updateName(name string) {
+	p.firstName = name
 }
