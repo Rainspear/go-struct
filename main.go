00ccd41e262f7dd5eac9400036c6b202f28ca199
Khang Phan <khang.phan@opswat.com>
2022-09-22 12:00:50 +0700

    add race condition example

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var counter int = 0
+
+var mu sync.Mutex
+
+func count(wg *sync.WaitGroup) {
+	mu.Lock()
+	counter += 1
+	mu.Unlock()
+	wg.Done()
+}
+
 func main() {
 	// lh := learnHttp{}
 	// li := learnInterface{}
 	// ls := learnStruct{}
 	// lm := learnMap{}
 	// lc := learnConcurrency{}
-	lg := learnGroup{}
+	// lg := learnGroup{}
+	// lr := learnRace{}
 	// lh.executeMain()
 	// li.executeMain()
 	// ls.executeMain()
 	// lm.executeMain()
 	// lc.executeMain()
-	lg.executeMain()
+	// lg.executeMain()
+	// lr.executeMain()
 
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go count(&wg)
+	}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go count(&wg)
+	}
+	wg.Wait()
+	fmt.Println("Counter:", counter)
 }
---
+++ race.go
@@ -0,0 +1,42 @@
+// Program with race condition
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+type learnRace struct{}
+
+var GFG = 0
+
+// This is the function we’ll run in every
+// goroutine. Note that a WaitGroup must
+// be passed to functions by pointer.
+func worker(wg *sync.WaitGroup) {
+	GFG = GFG + 1
+
+	// On return, notify the
+	// WaitGroup that we’re done.
+	wg.Done()
+}
+func (learnRace) executeMain() {
+
+	// This WaitGroup is used to wait for
+	// all the goroutines launched here to finish.
+	var w sync.WaitGroup
+
+	// Launch several goroutines and increment
+	// the WaitGroup counter for each
+	for i := 0; i < 1000; i++ {
+		w.Add(1)
+		go worker(&w)
+	}
+	// Block until the WaitGroup counter
+	// goes back to 0; all the workers
+	// notified they’re done.
+	fmt.Println("GoRoutine:", runtime.NumGoroutine())
+	w.Wait()
+	fmt.Println("Value of x", GFG)
+}
